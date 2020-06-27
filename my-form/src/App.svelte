e712489492cc7c4a69f81c2ca842adeecf2997a3
alfredo gonzalez <alfredo.gon195@gmail.com>
2020-06-27 11:27:43 -0700

    set up form

--- my-form/src/App.svelte
+++ my-form/src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+  // JS here
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  /* CSS here*/
 </style>
+
+<!-- HTML here-->
+
+<main>
+  <form>
+
+  <div class="field">
+  <input type="email" name= "email" class="input" placeholder=""/>
+  <label for="email" class="label">Email</label>
+  </div>
+
+   <div class="field">
+  <input type="password" name= "email" class="input" placeholder=""/>
+  <label for="password" class="label">Password</label>
+  </div>
+  
+  </form>
+</main>
