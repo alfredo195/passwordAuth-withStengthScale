fc0ee34c7f453281593aeb0a3e272f522a2e8f6d
alfredo gonzalez <alfredo.gon195@gmail.com>
2020-06-27 11:58:44 -0700

    border animation

--- my-form/src/App.svelte
+++ my-form/src/App.svelte
@@ -3,7 +3,67 @@
 </script>
 
 <style>
-  /* CSS here*/
+  form {
+	  --text-color: #afafaf;
+	  max-width: 500px;
+  }
+
+  .field {
+	  width: 100%;
+	  position: relative;
+	  border-bottom: 2px dashed var(--text-color);
+	  margin: 4rem auto 1rem;
+  }
+
+  .label {
+	  color: var(--text-color);
+	  font-size: 1.2rem;
+  }
+
+  .input{
+	  outline: none;
+	  border: none;
+	  overflow: hidden;
+	  margin: 0;
+	  width: 100%;
+	  padding: 0.25rem 0;
+	  background: none;
+	  color: white;
+	  font-size: 1.2rem;
+	  font-weight: bold;
+  }
+
+  .input:valid{
+	  color: yellowgreen
+  }
+
+  .input:invalid{
+	  color: orangered
+  }
+
+  /* border animation*/
+
+  .field::after {
+    content: "";
+    position: relative;
+    display: block;
+    height: 4px;
+    width: 100%;
+    background: #d16dff;
+    transform: scaleX(0);
+    transform-origin: 0%;
+    opacity: 0;
+    transition: all 500ms ease;
+    top: 2px;
+  }
+  .field:focus-within {
+    border-color: transparent;
+  }
+  .field:focus-within::after {
+    transform: scaleX(1);
+    opacity: 1;
+  }
+
 </style>
 
 <!-- HTML here-->
